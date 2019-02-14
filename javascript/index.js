dfdbf5a61ba0ed9d296f0248214b23753bf78ea8
Ami Heines <ami.heines@gmail.com>
2019-02-14 14:30:40 +0200

    help debug with console.log

--- javascript/index.js
+++ javascript/index.js
@@ -33,7 +33,7 @@ function createBoard(){
                     table=Math.floor(y/3)+7;
                     break;
             }
-            $("#outR"+x).append("<td id=\"int"+x+""+y+"\" class=\"int tableHight"+miniTablehight+" tableRow"+miniTableRow+" Row"+x+" Hight"+y+" table"+table+"\">XX</tr")
+            $("#outR"+x).append("<td id=\"int"+x+""+y+"\" class=\"int tableHight"+miniTablehight+" tableRow"+miniTableRow+" Row"+x+" Hight"+y+" table"+table+"\"></tr")
         }
     }
 }
@@ -42,21 +42,22 @@ function fillBoard(int){
     while(cnt<int){
         var random=Math.floor(Math.random()*88);
         var fill=Math.floor(Math.random()*9)+1;
+        console.log('fillBoard cnt, random, fill:',cnt, random, fill);
         var id="int"+random;
         if($("#"+id).hasClass("Full")){
+            console.log("#"+id+" hasClass-Full --> continue");
             continue;
-        }
-        else{
+        }else{
             var numRow=Math.floor(random/10)
             if(checkRow(numRow,fill)){
+                console.log("checkRow numRow="+numRow+",fill --> continue");
                 continue;
-            }
-            else{
+            }else{
                 var numLine=Math.floor(random/10)
                 if(checkLine(numLine,fill)){
+                    console.log("checkLine numLine="+numLine+",fill --> continue");
                     continue;
-                }
-                else{
+                }else{
                     var numTable=0;
                     switch(Math.floor(numRow/3)){
                         case 0:
@@ -71,9 +72,9 @@ function fillBoard(int){
                     }
                     numTable=numTable+numLine
                     if(checkTable(numTable,fill)){
+                        console.log("checkTable numTable="+numTable+",fill --> continue");
                         continue;
-                    }
-                    else{
+                    }else{
                         $("#"+id).html(fill)
                         $("#"+id).toggleClass("Full")
                         cnt++;
